f8cafbb5a0af82a7e06dfd531107788c9e0fcc8b
objectified <objectified@gmail.com>
2017-05-29 15:11:18 +0200

    initial commit

---
+++ src/main/java/com/angryobjects/test/App.java
@@ -0,0 +1,13 @@
+package com.angryobjects.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
